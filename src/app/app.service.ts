e3e6a3e6ec67e222bb5ed2b49de374a7704a1ee8
rajeshgreek <rajesh.purushothaman@greeksoft.co.in>
2021-02-25 14:02:21 +0530

    Initial commit

---
+++ src/app/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@angular/core";
+import { interval, of } from "rxjs";
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AppService {}
---
+++ src/app/mylist.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+import { Subject } from "rxjs";
+import { AppService } from "./app.service";
+
+@Component({
+  selector: "app-mylist",
+  template: ``,
+  styles: []
+})
+export default class MylistComponent {
+  private myData = [];
+}
